0f83fa32c4a418987d684f469b1ad640ef4a68d7
lregnier <leonardoregnier@gmail.com>
2019-05-27 16:46:05 -0300

    deferred-resolvers

--- src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -1,6 +1,6 @@
 package com.howtographql.scala.sangria
 
-import DBSchema._
+import com.howtographql.scala.sangria.DBSchema._
 import com.howtographql.scala.sangria.models._
 import slick.jdbc.H2Profile.api._
 
@@ -9,10 +9,6 @@ import scala.concurrent.Future
 class DAO(db: Database) {
 
   def allLinks: Future[Seq[Link]] = db.run(Links.result)
-
-  def getLink(id: Int): Future[Option[Link]] = db.run(
-    Links.filter(_.id === id).result.headOption
-  )
 
   def getLinks(ids: Seq[Int]): Future[Seq[Link]] = db.run(
     Links.filter(_.id inSet ids).result
