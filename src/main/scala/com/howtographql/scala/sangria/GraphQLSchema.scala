c971a15e36e085f14bc1fcbba66728057188c129
lregnier <leonardoregnier@gmail.com>
2019-05-27 16:21:15 -0300

    handling-arguments

--- src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -8,24 +8,27 @@ import sangria.macros.derive._
 
 object GraphQLSchema {
 
-  // 1
-  val LinkType = ObjectType[Unit, Link](
-    "Link",
-    fields[Unit, Link](
-      Field("id", IntType, resolve = _.value.id),
-      Field("url", StringType, resolve = _.value.url),
-      Field("description", StringType, resolve = _.value.description)
-    )
-  )
+  implicit val LinkType = deriveObjectType[Unit, Link]()
 
-  // 2
+  val Id = Argument("id", IntType)
+  val Ids = Argument("ids", ListInputType(IntType))
+
   val QueryType = ObjectType(
     "Query",
     fields[MyContext, Unit](
-      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks)
+      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks),
+      Field("link", //1
+        OptionType(LinkType), //2
+        arguments = Id :: Nil, //3
+        resolve = c => c.ctx.dao.getLink(c.arg[Int]("id")) //4
+      ),
+      Field("links", //1
+        ListType(LinkType), //2
+        arguments = Ids :: Nil, //3
+        resolve = c => c.ctx.dao.getLinks(c.arg[Seq[Int]]("ids")) //4
+      )
     )
   )
 
-  // 3
   val SchemaDefinition = Schema(QueryType)
 }
